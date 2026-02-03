b2e55a489d50aa083b4111f0f74fb9a538f68efa
Guluma Tafa <gulumabest694@gmail.com>
2026-02-03 22:23:29 +0300

    Fix comment on navigateButtonBar method
    Corrected a comment for clarity.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
   // this method will update the selected index
-  // when the users taps on the bottom navigation bar
+  // when the users taps on the bottom navigation ba
   void navigateButtonBar(int index) {
     setState(() {
       _selectedIndex = index;
